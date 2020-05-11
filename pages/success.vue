30468eb51df9d003c315370c83d324dc18420fb0
yansai <1114281828@qq.com>
2020-05-11 17:17:31 +0800

    update

--- pages/success.vue
+++ pages/success.vue
@@ -2,7 +2,7 @@
   <div class="success-page text-center text-black">
     <i class="el-icon-circle-check"></i>
     <p class="fs-24 p-t-30" >交卷成功</p>
-    <p class="p-t-25">拥有知识改变命运，拥有理想改变态度</p>
+    <p class="p-t-25 text-red text-bold">拥有知识改变命运，拥有理想改变态度</p>
   </div>
 </template>
 
